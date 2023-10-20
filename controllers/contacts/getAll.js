884dfdd8146b39e7774397f41f7be00ab2e5bbdd
Pavel Pereverziev <pereverzev.pavel1975@gmail.com>
2023-10-20 16:12:38 +0300

    added pagination and favourite filter

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,7 +2,24 @@ const Contact = require('../../models/contact');
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  res.json(await Contact.find({ owner }, '-createdAt -updatedAt'));
+  const { page = 1, limit = 10, favorite = false } = req.query;
+  const skip = (page - 1) * limit;
+  console.log(favorite);
+  if (favorite) {
+    res.status(200).json(
+      await Contact.find({ owner, favorite: true }, '-createdAt -updatedAt', {
+        skip,
+        limit,
+      })
+    );
+  } else {
+    res.status(200).json(
+      await Contact.find({ owner }, '-createdAt -updatedAt', {
+        skip,
+        limit,
+      })
+    );
+  }
 };
 
 module.exports = getAll;
