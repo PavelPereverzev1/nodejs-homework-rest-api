d8b87a43c1314a0f573aa55bb0e6b89073021e96
Pavel Pereverziev <pereverzev.pavel1975@gmail.com>
2023-10-21 13:03:27 +0300

    fix issues

--- controllers/contacts/deleteById.js
+++ controllers/contacts/deleteById.js
@@ -2,7 +2,9 @@ const Contact = require('../../models/contact');
 const { HttpError } = require('../../helpers');
 
 const deleteById = async (req, res) => {
-  const contact = await Contact.findByIdAndDelete(req.params.contactId);
+  const id = req.params.contactId;
+  const { _id: owner } = req.user;
+  const contact = await Contact.findOneAndDelete({ _id: id, owner: owner });
   if (!contact) {
     throw HttpError(404, 'Not found');
   }
--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,23 +3,14 @@ const Contact = require('../../models/contact');
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 10, favorite = false } = req.query;
+  const filter = favorite ? { owner, favorite } : { owner };
   const skip = (page - 1) * limit;
-  console.log(favorite);
-  if (favorite) {
-    res.status(200).json(
-      await Contact.find({ owner, favorite: true }, '-createdAt -updatedAt', {
-        skip,
-        limit,
-      })
-    );
-  } else {
-    res.status(200).json(
-      await Contact.find({ owner }, '-createdAt -updatedAt', {
-        skip,
-        limit,
-      })
-    );
-  }
+  res.status(200).json(
+    await Contact.find(filter, '-createdAt -updatedAt', {
+      skip,
+      limit,
+    })
+  );
 };
 
 module.exports = getAll;
