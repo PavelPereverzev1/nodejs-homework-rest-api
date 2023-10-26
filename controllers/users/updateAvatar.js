5f5d037f296dfaa0ab0d905493d74298593045f3
Pavel Pereverziev <pereverzev.pavel1975@gmail.com>
2023-10-26 19:24:14 +0300

    fix possible name conflict

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -8,10 +8,11 @@ const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
+  const fileName = `${_id}_${originalname}`;
   await resizeAvatar(tempUpload);
-  const resultUpload = path.join(avatarsDir, originalname);
+  const resultUpload = path.join(avatarsDir, fileName);
   await fs.rename(tempUpload, resultUpload);
-  const avatarURL = path.join('avatars', originalname);
+  const avatarURL = path.join('avatars', fileName);
   await User.findByIdAndUpdate(_id, { avatarURL });
 
   res.json({
