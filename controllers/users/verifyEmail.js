db12e8a4abc41b22d83823fc17eead57930fc4c7
Pavel Pereverziev <pereverzev.pavel1975@gmail.com>
2023-11-02 22:52:02 +0200

    fix bug

--- controllers/users/verifyEmail.js
+++ controllers/users/verifyEmail.js
@@ -3,13 +3,15 @@ const { HttpError } = require('../../helpers');
 
 const verifyEmail = async (req, res) => {
   const { verificationToken } = req.params;
+  console.log(req.params);
+  console.log(verificationToken);
   const user = await User.findOne({ verificationToken });
 
   if (!user) {
     throw HttpError(401, 'Email not found');
   }
   await User.findByIdAndUpdate(user._id, {
-    verfy: true,
+    verify: true,
     verificationToken: '',
   });
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/register', validateBody(schemas.registerSchema), ctrl.register);
 
-router.get('/verify/:verificatonToken', ctrl.verifyEmail);
+router.get('/verify/:verificationToken', ctrl.verifyEmail);
 
 router.post(
   '/verify',
